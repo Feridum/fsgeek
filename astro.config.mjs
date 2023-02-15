c94ddca83be3f7a7c5d6fa3748fd22e02c5dc733
Aleksander Patschek <aleksander.patschek@tsh.io>
2023-02-15 06:58:24 +0100

    fix(sitemap): fix sitemap main url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com/', trailingSlash: 'always',
+  site: 'https://fsgeek.pl/', trailingSlash: 'always',
   experimental: {
     integrations: true
   },

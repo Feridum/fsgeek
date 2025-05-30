2376bc6fef20ee29481ea887cd2df9fd52f467f6
Aleksander Patschek <aleksander.patschek@tsh.io>
2025-05-30 08:18:34 +0200

    feat(post): added cta to aifullstack

--- src/partials/BlogPost.tsx
+++ src/partials/BlogPost.tsx
@@ -13,6 +13,24 @@ type IBlogPostProps = {
 
 const BlogPost = (props: IBlogPostProps) => (
   <Section>
+    <div
+      className={
+        'mb-8 rounded-lg bg-blue-50 p-4 text-center text-sm text-blue-800'
+      }
+    >
+      Interesujesz sie budowaniem aplikacji AI? Chcesz zostać AI Fullstack?
+      Odwiedź{' '}
+      <a
+        href="https://aifullstack.pl/blog"
+        target="_blank"
+        className="font-extrabold underline hover:text-blue-600"
+      >
+        aifullstack.pl
+      </a>{' '}
+      i odkryj kompleksowe materiały, praktyczne poradniki oraz inspiracje,
+      które krok po kroku wprowadzą Cię w świat tworzenia aplikacji AI.
+    </div>
+
     <PostHeader content={props.frontmatter} author={AppConfig.author} />
 
     <PostContent content={props.frontmatter} headings={props.headings}>
